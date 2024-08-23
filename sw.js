7360acb7aacbdc96560613f5bf76238a1fb2ce7c
Harry <gardiner_harry@yahoo.com>
2024-08-23 17:08:18 +0100

    Changed required value to match forked repo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var GHPATH = '/github-page-pwa';
-var APP_PREFIX = 'gppwa_';
+var GHPATH = '/shopping-pwa';
+var APP_PREFIX = 'shp_';
 var VERSION = 'version_002';
 var URLS = [    
   `${GHPATH}/`,
